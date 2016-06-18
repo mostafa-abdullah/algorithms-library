64520f4353944a397826097960edfc1c89127520
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2016-06-18 04:29:45 +0200

    Added printing eurlerian tour

--- graphs/EurlerianGraph.java
+++ graphs/EurlerianGraph.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public class EurlerianGraph {
 	static int N;
-	static ArrayList<Integer> adjList[];
+	static ArrayList<Pair> adjList[];
 	
 	
 	static boolean hasEurlerianTour()
 	{
-		for(ArrayList<Integer> l : adjList)
+		for(ArrayList l : adjList)
 			if(l.size() % 2 == 1)
 				return false;
 		return true;
@@ -18,7 +18,7 @@ public class EurlerianGraph {
 	static boolean hasEurlerianPath()
 	{
 		int odd = 0;
-		for(ArrayList<Integer> l : adjList)
+		for(ArrayList l : adjList)
 			if(l.size() % 2 == 1)
 				odd++;
 		return odd == 0 || odd == 2;
@@ -27,5 +27,35 @@ public class EurlerianGraph {
 	static boolean isEurlerian()
 	{
 		return hasEurlerianPath();
+	}
+	
+	static ArrayList<Integer> tour;
+	static void getEurlerianTour(int u)
+	{
+		for(Pair p : adjList[u])
+			if(p.valid)
+			{
+				p.valid = false;
+				for(Pair p2 : adjList[p.v])
+				
+					if(p2.valid && p2.v == u)
+					{
+						p2.valid = false;
+						break;
+					}
+				tour.add(u);
+				getEurlerianTour(p.v);
+			}
+	}
+	
+	static class Pair
+	{
+		int v;
+		boolean valid;
+		public Pair(int x, boolean y)
+		{
+			v = x;
+			valid = y;
+		}
 	}
 }
